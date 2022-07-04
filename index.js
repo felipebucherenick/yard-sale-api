e92deafbd0147eb615758632607703e9a534c2d7
Felipe Bucherenick <felipebucherenick@gmail.com>
2022-07-04 18:07:13 -0400

    add config to deploy

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routers');
 const {
   boomErrorHandler,
   errorHandler,
 } = require('./middlewares/error.handler');
-const port = 3003;
+const port = process.env.PORT || 3003;
 const app = express();
 
 app.use(express.json());
+
+const whitelist = ['https://yard-sale-admin.vercel.app/'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origin not allowed'));
+    }
+  },
+};
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Yard Sale API');
